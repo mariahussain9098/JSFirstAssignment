c3b6ee69c88a5b740c58d6e4598e8e5b1ba831e7
wma9969 <49679579+wma9969@users.noreply.github.com>
2023-10-06 16:19:45 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,19 @@
+// adding some JavaScript code to app.js file
+alert("This is my app.js file, and I linked this file to practice exercise 1.2 (index.html file)");
+
+
+let a = 10; //set a value of 10 in variable a
+
+
+console.log(a); // this will print 10 in the browser console.
+
+/*
+  This is a multiline comment
+  and it can span multiple lines.
+  In this specific file we create and alert fucntion and along that we declare a varialbe named 'a' and its value is 10. Also we print that variable in console.log.
+*/
+
+// console.log("This console is for comment purpose");
+
+// Self Check Quiz
+
